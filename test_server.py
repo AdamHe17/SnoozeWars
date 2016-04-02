e6cf3e01d8eab0f771b4d6fca6a01527107b4bb5
Adam He <shuaihe2018@u.northwestern.edu>
2016-04-02 16:41:31 -0400

    many changes
    too many

--- test_server.py
+++ test_server.py
@@ -35,8 +35,11 @@ sock = socket.socket()         	# Create a socket object
 host = socket.gethostname() 	# Get local machine name
 port = 5000                		# Reserve a port for your service.
 sock.bind((host, port))        	# Bind to the port
-
+client_port = 45000
 sock.listen(5)                	# Now wait for client connection.
+
+first = True;
+
 while True:
 	client, addr = sock.accept()     # Establish connection with client.
 	print 'Got connection from', addr
@@ -53,6 +56,19 @@ while True:
 		for p in users:
 			print p.addr, p.time, p.partner
 	elif data == 'snooze':
-		client.send(str(randint(1, 10)))
+		if first:
+			first = False
+		else:
+			for u in users:
+				print u.get_addr()
+				print addr[0]
+				if u.get_addr() == addr[0]:
+					tmp = socket.socket()
+					print 'tmp socket initialized'
+					tmp.connect(((u.get_partner()).get_addr(), client_port))
+					print 'tmp connection created to ' + u.get_partner().get_addr() + ' on port ' + str(client_port)
+					tmp.send('Unsnoozed!')
+					print 'message sent'
+		#client.send(str(randint(1, 10)))
 	print 'closing socket'
-	client.close()                	# Close the connection
+	#client.close()                	# Close the connection
